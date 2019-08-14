6c1553f46bc4138620500453406702f0a5e48730
abelsco <abelsmenezes@gmail.com>
2019-08-13 21:46:57 -0300

    JSON primeira tentativa

--- embarcadoExplocao.ino
+++ embarcadoExplocao.ino
@@ -1,5 +1,7 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <ArduinoJson.h>
+
 #define TIMEOUT 1000
 
 /* PINOS (FAIXA-OPERACAO)[INTERVALO]
@@ -24,10 +26,12 @@ long antWebServiceMillis = 0;
 const long webServiceMillisInterval = 30;
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(192, 168, 15, 177);
+// IPAddress ip(192, 168, 15, 177);
+IPAddress ip(127, 0, 0, 1);
 EthernetServer server(80);
 
-void printSerialAmbiente() {
+void printSerialAmbiente()
+{
   Serial.print("========\nTemperatura: ");
   Serial.print(atualTemp, 0);
   Serial.print("ºC\nUmidade: ");
@@ -43,7 +47,8 @@ void printSerialAmbiente() {
   Serial.println(" J");
 }
 
-void printWebAmbiente(EthernetClient client) {
+void setWebAmbiente(EthernetClient client)
+{
   client.print("========<br />Temperatura: ");
   client.print(atualTemp, 0);
   client.print("ºC<br />Umidade: ");
@@ -59,7 +64,52 @@ void printWebAmbiente(EthernetClient client) {
   client.println(" J<br />");
 }
 
-void setAmbienteSimulado() {
+void printWebAmbiente(EthernetClient client)
+{
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close"); // the connection will be closed after completion of the response
+  //client.println("Refresh: 2");  // refresh the page automatically every 5 sec
+  client.println();
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  // output the value of each analog input pin
+  setWebAmbiente(client);
+  client.println("</html>");
+}
+
+void setJsonAmbiente(EthernetClient client, StaticJsonDocument<500> doc)
+{
+  JsonArray pressao = doc.createNestedArray("pressao");
+  pressao.add(atualPre);
+  JsonArray temperatura = doc.createNestedArray("temperatura");
+  temperatura.add(atualTemp);
+  JsonArray conceOxi = doc.createNestedArray("conceOxi");
+  conceOxi.add(atualOxi);
+  JsonArray fonteIg = doc.createNestedArray("fonteIg");
+  fonteIg.add(atualIg);
+  JsonArray umidade = doc.createNestedArray("umidade");
+  umidade.add(atualUmi);
+  JsonArray concePo = doc.createNestedArray("concePo");
+  concePo.add(atualPoeira);
+  serializeJson(doc, Serial);
+  serializeJsonPretty(doc, client);
+}
+
+void printJsonAmbiente(EthernetClient client)
+{  
+  StaticJsonDocument<500> doc;
+  client.println(F("HTTP/1.0 200 OK"));
+  client.println(F("Content-Type: application/json"));
+  client.println(F("Connection: close"));
+  client.print(F("Content-Length: "));  
+  setJsonAmbiente(client, doc);
+  client.println(measureJsonPretty(doc));
+  client.println();
+}
+
+void setAmbienteSimulado()
+{
   atualTemp = analogRead(A0);
   atualUmi = analogRead(A1);
   atualPre = analogRead(A2);
@@ -75,38 +125,39 @@ void setAmbienteSimulado() {
   atualIg = atualIg * 2 / 1023;
 }
 
-void webService() {
+void webService()
+{
   EthernetClient client = server.available();
-  if (client) {
-    client.setConnectionTimeout(TIMEOUT);  // set the timeout duration for client.connect() and client.stop()
+  if (client)
+  {
+    StaticJsonDocument<500> doc;
+    client.setConnectionTimeout(TIMEOUT); // set the timeout duration for client.connect() and client.stop()
     Serial.println("new client");
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
+    while (client.connected())
+    {
+      if (client.available())
+      {
         char c = client.read();
         Serial.write(c);
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
+        if (c == '\n' && currentLineIsBlank)
+        {
           // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          //client.println("Refresh: 2");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          printWebAmbiente(client);
-          client.println("</html>");
+          // printWebAmbiente(client);
+          printJsonAmbiente(client);
           break;
         }
-        if (c == '\n') {
+        if (c == '\n')
+        {
           // you're starting a new line
           currentLineIsBlank = true;
-        } else if (c != '\r') {
+        }
+        else if (c != '\r')
+        {
           // you've gotten a character on the current line
           currentLineIsBlank = false;
         }
@@ -120,13 +171,15 @@ void webService() {
   }
 }
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(57600);
   // You can use Ethernet.init(pin) to configure the CS pin
   //Ethernet.init(10);  // Most Arduino shields
 
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("=====================================================================");
   Serial.println("Ethernet WebServer Example");
 
@@ -134,33 +187,42 @@ void setup() {
   Ethernet.begin(mac, ip);
 
   // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+  if (Ethernet.hardwareStatus() == EthernetNoHardware)
+  {
     Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true);
-  } else if (Ethernet.linkStatus() == LinkOFF) {
+    while (true)
+      ;
+  }
+  else if (Ethernet.linkStatus() == LinkOFF)
+  {
     Serial.println("Ethernet cable is not connected.");
-  } else if (Ethernet.hardwareStatus() == EthernetW5100) {
+  }
+  else if (Ethernet.hardwareStatus() == EthernetW5100)
+  {
     Serial.println("W5100 Ethernet controller detected.");
     // start the server
     server.begin();
-    Serial.print("server is at ");    
+    Serial.print("server is at ");
     Serial.println(Ethernet.localIP());
-  Serial.println("=====================================================================\n");   
+    Serial.println("=====================================================================\n");
   }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   unsigned long currentMillisWebService = millis();
-  if (currentMillisWebService - antWebServiceMillis > webServiceMillisInterval) {
+  if (currentMillisWebService - antWebServiceMillis > webServiceMillisInterval)
+  {
     antWebServiceMillis = currentMillisWebService;
     webService();
   }
-  
+
   unsigned long currentMillisAmbiente = millis();
-  if (currentMillisAmbiente - antAmbienteMillis > ambienteMillisInterval) {
+  if (currentMillisAmbiente - antAmbienteMillis > ambienteMillisInterval)
+  {
     antAmbienteMillis = currentMillisAmbiente;
     setAmbienteSimulado();
-    //printSerialAmbiente();
+    // printSerialAmbiente();
   }
 }
