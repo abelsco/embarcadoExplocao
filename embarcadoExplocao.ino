1b601cad769f88af9eb00c1e6ef3ca2364e4d6f9
abelsco <abelsmenezes@gmail.com>
2019-08-29 18:28:39 -0300

    Update

--- embarcadoExplocao.ino
+++ embarcadoExplocao.ino
@@ -1,6 +1,6 @@
-#include <SPI.h>
-#include <Ethernet.h>
 #include <ArduinoJson.h>
+#include <Ethernet.h>
+#include <SPI.h>
 
 #define TIMEOUT 1000
 #define MILLS_AMBIENTE 3
@@ -29,28 +29,16 @@ float fonteV = 1;
 float umidadeV = 1;
 float concepoV = 1;
 
-int situTemperatura = 0;
-int situFonte = 0;
-int situPressao = 0;
-int situOxi = 0;
-int situPo = 0;
-int situUmidade = 0;
-int situSilo = 0;
-
 long antAmbienteMillis = 0;
 long antWebServiceMillis = 0;
+bool DHCP = false;
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(192, 168, 0, 177);
-IPAddress myDns(8, 8, 8, 8);
-IPAddress gateway(192, 168, 0, 1);
-IPAddress subnet(255, 255, 255, 0);
-EthernetServer server(80);
+// IPAddress ip(192,168,16,177);
+EthernetServer server(3000);
 
-void defParametros(String tipoGrao)
-{
-  if (tipoGrao == "Açúcar em pó")
-  {
+void defParametros(String tipoGrao) {
+  if (tipoGrao == "Açúcar em pó") {
     pressaoV = 7.7;
     temperaturaV = 370;
     conceoxiV = 21;
@@ -59,8 +47,7 @@ void defParametros(String tipoGrao)
     concepoV = 45;
   }
 
-  else if (tipoGrao == "Alho desidratado")
-  {
+  else if (tipoGrao == "Alho desidratado") {
     pressaoV = 4;
     temperaturaV = 360;
     conceoxiV = 21;
@@ -69,8 +56,7 @@ void defParametros(String tipoGrao)
     concepoV = 10;
   }
 
-  else if (tipoGrao == "Arroz")
-  {
+  else if (tipoGrao == "Arroz") {
     pressaoV = 3.3;
     temperaturaV = 510;
     conceoxiV = 21;
@@ -79,8 +65,7 @@ void defParametros(String tipoGrao)
     concepoV = 85;
   }
 
-  else if (tipoGrao == "Casca de arroz")
-  {
+  else if (tipoGrao == "Casca de arroz") {
     pressaoV = 7.7;
     temperaturaV = 450;
     conceoxiV = 17;
@@ -89,8 +74,7 @@ void defParametros(String tipoGrao)
     concepoV = 55;
   }
 
-  else if (tipoGrao == "Semente de arroz(casca)")
-  {
+  else if (tipoGrao == "Semente de arroz(casca)") {
     pressaoV = 4.3;
     temperaturaV = 490;
     conceoxiV = 12;
@@ -99,8 +83,7 @@ void defParametros(String tipoGrao)
     concepoV = 45;
   }
 
-  else if (tipoGrao == "Proteína de soja")
-  {
+  else if (tipoGrao == "Proteína de soja") {
     pressaoV = 6.9;
     temperaturaV = 540;
     conceoxiV = 12;
@@ -109,8 +92,7 @@ void defParametros(String tipoGrao)
     concepoV = 40;
   }
 
-  else if (tipoGrao == "Farinha de soja")
-  {
+  else if (tipoGrao == "Farinha de soja") {
     pressaoV = 6.6;
     temperaturaV = 550;
     conceoxiV = 15;
@@ -119,8 +101,7 @@ void defParametros(String tipoGrao)
     concepoV = 60;
   }
 
-  else if (tipoGrao == "Trigo bruto")
-  {
+  else if (tipoGrao == "Trigo bruto") {
     pressaoV = 5;
     temperaturaV = 500;
     conceoxiV = 12;
@@ -129,8 +110,7 @@ void defParametros(String tipoGrao)
     concepoV = 0.5;
   }
 
-  else if (tipoGrao == "Farinha de trigo")
-  {
+  else if (tipoGrao == "Farinha de trigo") {
     pressaoV = 7;
     temperaturaV = 440;
     conceoxiV = 15;
@@ -139,8 +119,7 @@ void defParametros(String tipoGrao)
     concepoV = 50;
   }
 
-  else if (tipoGrao == "Trigo Cereal")
-  {
+  else if (tipoGrao == "Trigo Cereal") {
     pressaoV = 9.2;
     temperaturaV = 430;
     conceoxiV = 13;
@@ -149,8 +128,7 @@ void defParametros(String tipoGrao)
     concepoV = 35;
   }
 
-  else if (tipoGrao == "Palha de trigo")
-  {
+  else if (tipoGrao == "Palha de trigo") {
     pressaoV = 8.2;
     temperaturaV = 470;
     conceoxiV = 13;
@@ -159,8 +137,7 @@ void defParametros(String tipoGrao)
     concepoV = 75;
   }
 
-  else if (tipoGrao == "Polvilho de trigo")
-  {
+  else if (tipoGrao == "Polvilho de trigo") {
     pressaoV = 7;
     temperaturaV = 430;
     conceoxiV = 12;
@@ -169,8 +146,7 @@ void defParametros(String tipoGrao)
     concepoV = 45;
   }
 
-  else if (tipoGrao == "Milho")
-  {
+  else if (tipoGrao == "Milho") {
     pressaoV = 8;
     temperaturaV = 400;
     conceoxiV = 13;
@@ -179,8 +155,7 @@ void defParametros(String tipoGrao)
     concepoV = 55;
   }
 
-  else if (tipoGrao == "Casca de milho cru")
-  {
+  else if (tipoGrao == "Casca de milho cru") {
     pressaoV = 8.7;
     temperaturaV = 410;
     conceoxiV = 17;
@@ -189,8 +164,7 @@ void defParametros(String tipoGrao)
     concepoV = 40;
   }
 
-  else if (tipoGrao == "Polvilho de milho")
-  {
+  else if (tipoGrao == "Polvilho de milho") {
     pressaoV = 10.2;
     temperaturaV = 390;
     conceoxiV = 11;
@@ -199,8 +173,7 @@ void defParametros(String tipoGrao)
     concepoV = 40;
   }
 
-  else if (tipoGrao == "Semente de milho")
-  {
+  else if (tipoGrao == "Semente de milho") {
     pressaoV = 8.9;
     temperaturaV = 450;
     conceoxiV = 12;
@@ -210,8 +183,7 @@ void defParametros(String tipoGrao)
     concepoV = 40;
   }
 }
-void printSerialAmbiente()
-{
+void printSerialAmbiente() {
   Serial.print("========\nTemperatura: ");
   Serial.print(atualTemp, 0);
   Serial.print("ºC\nUmidade: ");
@@ -227,8 +199,7 @@ void printSerialAmbiente()
   Serial.println(" J");
 }
 
-void setWebAmbiente(EthernetClient client)
-{
+void setWebAmbiente(EthernetClient client) {
   client.print("========<br />Temperatura: ");
   client.print(atualTemp, 0);
   client.print("ºC<br />Umidade: ");
@@ -244,12 +215,13 @@ void setWebAmbiente(EthernetClient client)
   client.println(" J<br />");
 }
 
-void printWebAmbiente(EthernetClient client)
-{
+void printWebAmbiente(EthernetClient client) {
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
-  client.println("Connection: close"); // the connection will be closed after completion of the response
-  //client.println("Refresh: 2");  // refresh the page automatically every 5 sec
+  client.println("Connection: close"); // the connection will be closed after
+  // completion of the response
+  // client.println("Refresh: 2");  // refresh the page automatically every 5
+  // sec
   client.println();
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
@@ -258,26 +230,25 @@ void printWebAmbiente(EthernetClient client)
   client.println("</html>");
 }
 
-void printJsonAmbiente(EthernetClient client)
-{
+void printJsonAmbiente(EthernetClient client) {
   StaticJsonDocument<260> doc;
   doc["pressao"] = atualPre;
-  doc["situPressao"] = situPressao;
+  doc["situPressao"] = atualUmi;
   doc["temperatura"] = atualTemp;
-  doc["situTemperatura"] = situTemperatura;
+  doc["situTemperatura"] = atualUmi;
   doc["conceOxi"] = atualOxi;
-  doc["situConceOxi"] = situOxi;
+  doc["situConceOxi"] = atualUmi;
   doc["fonteIg"] = atualIg;
-  doc["situFonteIg"] = situFonte;
+  doc["situFonteIg"] = atualUmi;
   doc["umidade"] = atualUmi;
-  doc["situUmidade"] = situUmidade;
+  doc["situUmidade"] = atualUmi;
   doc["concePo"] = atualPoeira;
-  doc["situConcePo"] = situPo;
-  doc["situaSilo"] = situSilo;
+  doc["situConcePo"] = atualUmi;
+  doc["situaSilo"] = atualUmi;
   client.println(F("HTTP/1.0 200 OK"));
-  client.println(F("GET GET /api/ambiente/ HTTP/1.0"));
+  client.println(F("Access-Control-Allow-Origin: *"));
   client.println(F("Content-Type: application/json"));
-  // client.println("Refresh: 1");  // refresh the page automatically every 5 sec
+  client.println(F("Accept-Encoding: gzip, deflate"));
   client.println(F("Connection: close"));
   client.print(F("Content-Length: "));
   client.println(measureJsonPretty(doc));
@@ -286,19 +257,7 @@ void printJsonAmbiente(EthernetClient client)
   serializeJson(doc, client);
 }
 
-void geraSitu()
-{
-  situTemperatura = (atualTemp / temperaturaV) * 100;
-  situFonte = atualIg / fonteV * 100;
-  situPressao = atualPre / pressaoV * 100;
-  situOxi = atualOxi / conceoxiV * 100;
-  situPo = atualPoeira / concepoV * 100;
-  situUmidade = atualUmi / umidadeV * 100;
-  situSilo = (situTemperatura + situFonte + situPressao + situOxi + situPo + situUmidade) / 6;
-}
-
-void setAmbienteSimulado()
-{
+void setAmbienteSimulado() {
   atualTemp = analogRead(A5);
   atualUmi = analogRead(A4);
   atualPre = analogRead(A3);
@@ -312,42 +271,32 @@ void setAmbienteSimulado()
   atualPoeira = atualPoeira * 100 / 1023;
   atualOxi = map(atualOxi, 0, 409, 0, 100);
   atualIg = atualIg * 2 / 1023;
-
-  defParametros("Alho desidratado");
-  geraSitu();
 }
 
-void resourceWebServer(EthernetClient client, String req)
-{
-  if (req == "GET /api/ambiente/ HTTP/1.1")
-  {
+void resourceWebServer(EthernetClient client, String req) {
+  if (req == "GET /api/ambiente/ HTTP/1.1") {
     printJsonAmbiente(client);
-  }
-  else if (req == "POST /api/ambiente/ HTTP/1.1")
-  {
+  } else if (req == "GET /api/ambiente/? HTTP/1.1") {
+    printJsonAmbiente(client);
+  } else {
     printSerialAmbiente();
   }
-  else
-  {
-    printWebAmbiente(client);
-  }
 }
 
-void webService()
-{
+void webService() {
   EthernetClient client = server.available();
-  if (client)
-  {
-    client.setConnectionTimeout(TIMEOUT); // set the timeout duration for client.connect() and client.stop()
+  if (client) {
+    client.setConnectionTimeout(TIMEOUT); // set the timeout duration for
+    // client.connect() and client.stop()
     Serial.println();
     Serial.print("[HOST]: ");
     Serial.println(client.remoteIP());
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     client.connected();
-    if (client.available())
-    {
+    if (client.available()) {
       String req = client.readStringUntil('\r');
+      // String req = client.readString();
       Serial.print("[HTTP-REQUEST]: ");
       Serial.println(req);
       // client.flush();
@@ -356,66 +305,51 @@ void webService()
       resourceWebServer(client, req);
     }
     // give the web browser time to receive the data
-    delay(3);
+    delay(1);
     // close the connection:
     client.stop();
     Serial.println("client disconnected");
   }
 }
 
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
   // You can use Ethernet.init(pin) to configure the CS pin
   Ethernet.init(10); // Most Arduino shields
 
-  while (!Serial)
-    continue;
-  Serial.println("=====================================================================");
-  Serial.println("Ethernet WebServer Example");
+  Serial.println(
+      "=====================================================================");
+  Serial.println("Ethernet WebServer");
 
   // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
-
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware)
-  {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true)
-      continue;
+  while (!DHCP) {
+    if (Ethernet.begin(mac) != 0) {
+      DHCP = true;
+    }
   }
-  else if (Ethernet.linkStatus() == LinkOFF)
-  {
-    Serial.println("Ethernet cable is not connected.");
-  }
-  else if (Ethernet.hardwareStatus() == EthernetW5100)
-  {
-    Serial.println("W5100 Ethernet controller detected.");
-    // start the server
-    server.begin();
-    Serial.print("server is at ");
-    Serial.println(Ethernet.localIP());
-    Serial.println("=====================================================================\n");
-  }
+  // start the WebServer
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+  server.begin();
+  Serial.println("==========================================================="
+                 "==========\n");
 }
 
-void loop()
-{
+void loop() {
   // webService();
   // put your main code here, to run repeatedly:
+
   unsigned long currentMillisWebService = millis();
-  if (currentMillisWebService - antWebServiceMillis > MILLS_WEBSERVICE)
-  {
+  if (currentMillisWebService - antWebServiceMillis > MILLS_WEBSERVICE) {
     antWebServiceMillis = currentMillisWebService;
     webService();
   }
 
   unsigned long currentMillisAmbiente = millis();
-  if (currentMillisAmbiente - antAmbienteMillis > MILLS_AMBIENTE)
-  {
+  if (currentMillisAmbiente - antAmbienteMillis > MILLS_AMBIENTE) {
     antAmbienteMillis = currentMillisAmbiente;
     setAmbienteSimulado();
-    //printSerialAmbiente();
+    // printSerialAmbiente();
   }
 }
